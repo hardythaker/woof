5d61c846b9f6e56ad6b1e0d0be50de78b913580b
hardythaker <hardik.thaker1994@gmail.com>
2020-10-10 18:33:13 +0530

    Completed till Features section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { SubscriptionComponent } from './components/subscription/subscription.component';
 import { AboutComponent } from './components/about/about.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +14,11 @@ import { AboutComponent } from './components/about/about.component';
     HeaderComponent,
     HomeComponent,
     SubscriptionComponent,
-    AboutComponent
+    AboutComponent,
+    FeaturesComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
